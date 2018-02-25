7792704b8b09e915d854272de36791f05b073641
Chris <chrisiivix@gmail.com>
2018-02-25 16:31:52 +1100

    updated user and spell controller with new auth model.

--- src/controllers/spellController.js
+++ src/controllers/spellController.js
@@ -46,76 +46,67 @@ function buildFindQuery(filters) {
 }
 
 // READ //
-// GETS A SINGLE SPELL FROM THE DATABASE
-router.get('/id/:id', function (req, res) {
-    spell.findById(req.params.id, function (err, spell) {
-        if (err) return res.status(500).send("There was a problem finding the spell.");
-        if (!spell) return res.status(404).send("No spell found.");
-        res.status(200).send(spell);
+// RETURNS ALL SPELLS IN THE DATABASE
+router.get('/', function (req, res, next) {
+    spell.find({}, function (err, spells) {
+        if (err) {
+            const err = new Error("There was a problem finding the spells.");
+            err.status = 500;
+            return next(err);
+        }
+
+        if (!spells) {
+            const err = new Error("No spells found.");
+            err.status = 404;
+            return next(err);
+        }
+
+        return res.status(200).send(spells);
     });
 });
 
-// GETS A SINGLE LIGHTLY LOADED SPELL FROM THE DATABASE
-router.get('/light/id/:id', function (req, res) {
-    spell.findById(req.params.id,
-        'name school level classes castingTime castingTimeDescription range rangeDescription components duration durationDescription',
-        function (err, spell) {
-            if (err) return res.status(500).send("There was a problem finding the spell.");
-            if (!spell) return res.status(404).send("No spell found.");
-            res.status(200).send(spell);
+// GETS A SINGLE SPELL FROM THE DATABASE
+router.get('/id/:id', function (req, res, next) {
+    spell.findById(req.params.id, function (err, spell) {
+        if (err) {
+            const err = new Error("There was a problem finding the spell.");
+            err.status = 500;
+            return next(err);
         }
-    );
-});
 
-// RETURNS ALL SPELLS IN THE DATABASE
-router.get('/', function (req, res) {
-    spell.find({}, function (err, spells) {
-        if (err) return res.status(500).send("There was a problem finding the Spells.");
-        res.status(200).send(spells);
+        if (!spell) {
+            const err = new Error("No spell found.");
+            err.status = 404;
+            return next(err);
+        }
+
+        return res.status(200).send(spell);
     });
 });
 
 // RETURNS ALL LIGHtLY LOADED SPELLS IN THE DATABASE
-router.get('/light', function (req, res) {
+router.get('/light', function (req, res, next) {
     spell.find({},
         'name school level classes castingTime castingTimeDescription range rangeDescription components duration durationDescription',
         function (err, spells) {
-            if (err) return res.status(500).send("There was a problem finding the Spells.");
-            res.status(200).send(spells);
+            if (err) {
+                const err = new Error("There was a problem finding the spells.");
+                err.status = 500;
+                return next(err);
+            }
+    
+            if (!spells) {
+                const err = new Error("No spells found.");
+                err.status = 404;
+                return next(err);
+            }
+    
+            return res.status(200).send(spells);
         }
     );
-});
-
-// RETURNS ALL SPELLS IN THE DATABASE WITH POSIBLE FILTERS
-router.get('/withfilters', function (req, res) {
-    spell.find({}, function (err, spells) {
-        if (err) return res.status(500).send("There was a problem finding the Spellsssss.");
-
-        const spellsWithFilters = {
-            filters: getAllPossibleFilters(spells),
-            spells: spells
-        }
-        return res.status(200).send(spellsWithFilters);
-    });
 });
 
 // RETURNS ALL LIGHTLY LOADED SPELLS IN THE DATABASE WITH POSIBLE FILTERS
-router.get('/light/withfilters', function (req, res) {
-    spell.find({},
-        'name school level classes castingTime castingTimeDescription range rangeDescription components duration durationDescription',
-        function (err, spells) {
-            if (err) return res.status(500).send("There was a problem finding the Spellsssss.");
-
-            const spellsWithFilters = {
-                filters: getAllPossibleFilters(spells),
-                spells: spells
-            }
-            return res.status(200).send(spellsWithFilters);
-        }
-    );
-});
-
-// RETURNS SPELLS WITH POSSIBLE FILTERS FROM SUPPLIED FILTERS INPUT
 // req: {
 //          filters {
 //              name: 'some spell'
@@ -127,26 +118,33 @@ router.get('/light/withfilters', function (req, res) {
 //          }
 //       }
 // Note, All properties in the filters object are optional.
-router.post('/withfilters', function (req, res) {
-    console.log(req.body);
+router.get('/light/withfilters', function (req, res, next) {
+    spell.find({},
+        'name school level classes castingTime castingTimeDescription range rangeDescription components duration durationDescription',
+        function (err, spells) {
+            if (err) {
+                const err = new Error("There was a problem finding the spells.");
+                err.status = 500;
+                return next(err);
+            }
+    
+            if (!spells) {
+                const err = new Error("No spells found.");
+                err.status = 404;
+                return next(err);
+            }
 
-    const filters = buildFindQuery(req.body);
-    console.log(filters);
-    spell.find(filters, function (err, spells) {
-        if (err) {
-            return res.status(500).send("There was a problem finding the Spells.");
+            const spellsWithFilters = {
+                filters: getAllPossibleFilters(spells),
+                spells: spells
+            }
+            return res.status(200).send(spellsWithFilters);
         }
-
-        const spellsWithFilters = {
-            filters: getAllPossibleFilters(spells),
-            spells: spells
-        }
-        return res.status(200).send(spellsWithFilters);
-    });
+    );
 });
 
 // RETURNS LIGHTLY LOADED SPELLS WITH POSSIBLE FILTERS FROM SUPPLIED FILTERS INPUT
-router.post('/light/withfilters', function (req, res) {
+router.post('/light/withfilters', function (req, res, next) {
     console.log(req.body);
 
     const filters = buildFindQuery(req.body);
@@ -155,7 +153,15 @@ router.post('/light/withfilters', function (req, res) {
         'name school level classes castingTime castingTimeDescription range rangeDescription components duration durationDescription',
         function (err, spells) {
             if (err) {
-                return res.status(500).send("There was a problem finding the Spells.");
+                const err = new Error("There was a problem finding the spells.");
+                err.status = 500;
+                return next(err);
+            }
+    
+            if (!spells) {
+                const err = new Error("No spells found.");
+                err.status = 404;
+                return next(err);
             }
 
             const spellsWithFilters = {
@@ -169,7 +175,7 @@ router.post('/light/withfilters', function (req, res) {
 
 // CREATE //
 // CREATES A NEW SPELL
-// router.post('/create', function (req, res) {
+// router.post('/create', function (req, res, next) {
 //     spell.create(req.body, function (err, spell) {
 //         if (err) return res.status(500).send("There was a problem adding the information to the database.");
 //         res.status(200).send(spell);
@@ -178,9 +184,14 @@ router.post('/light/withfilters', function (req, res) {
 // });
 
 // CREATES AN ARRAY SPELLS
-router.post('/create/batch', function (req, res) {
+router.post('/create/batch', function (req, res, next) {
     spell.insertMany(req.body.spells, function (err, spells) {
-        if (err) return res.status(500).send("There was a problem adding the information to the database.");
+        if (err) {
+            const err = new Error("There was a problem adding the information to the database.");
+            err.status = 500;
+            return next(err);
+        }
+
         res.status(200).send(spells);
     }
     );
@@ -188,30 +199,49 @@ router.post('/create/batch', function (req, res) {
 
 // UPDATE //
 // UPDATES A SINGLE SPELL IN THE DATABASE
-// router.put('/:id', function (req, res) {
-//     spell.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, spell) {
-//         if (err) return res.status(500).send("There was a problem updating the spell.");
-//         res.status(200).send(spell);
-//     });
-// });
+router.put('/:id', function (req, res, next) {
+    spell.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, spell) {
+        if (err) {
+            const err = new Error("There was a problem updating the spell.");
+            err.status = 500;
+            return next(err);
+        }
 
-// DELETE //
-// DELETES A SPELL FROM THE DATABASE
-// router.delete('/:id', function (req, res) {
-//     spell.findByIdAndRemove(req.params.id, function (err, spell) {
-//         if (err) return res.status(500).send("There was a problem deleting the spell.");
-//         res.status(200).send("spell " + spell.name + " was deleted.");
-//     });
-// });
+        if (!spell) {
+            const err = new Error("No spells found to update.");
+            err.status = 404;
+            return next(err);
+        }
 
-// DELETES ALL SPELLS FROM THE DATABASE
-router.delete('/', function (req, res) {
-    spell.deleteMany({}, function (err, spell) {
-        if (err) return res.status(500).send("There was a problem deleting the spell.");
-        res.status(200).send("spell " + spell.name + " was deleted.");
+        res.status(200).send(spell);
     });
 });
 
+// DELETE //
+// DELETES A SPELL FROM THE DATABASE
+router.delete('/:id', function (req, res, next) {
+    spell.findByIdAndRemove(req.params.id, function (err, spell) {
+        if (err) {
+            const err = new Error("There was a problem removing the spell.");
+            err.status = 500;
+            return next(err);
+        }
 
+        res.status(200).send("Spell was deleted.");
+    });
+});
+
+// DELETES ALL SPELLS FROM THE DATABASE
+router.delete('/', function (req, res, next) {
+    spell.deleteMany({}, function (err, spell) {
+        if (err) {
+            const err = new Error("There was a problem removing the spells.");
+            err.status = 500;
+            return next(err);
+        }
+
+        res.status(200).send("spell " + spell.name + " was deleted.");
+    });
+});
 
 export default router;
