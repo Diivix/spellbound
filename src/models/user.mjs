9bbebc85925725a37b47c1382538fe50462e040f
Chris <chrisiivix@gmail.com>
2018-05-05 16:53:55 +1000

    Removed uneeded comments.

--- src/models/user.mjs
+++ src/models/user.mjs
@@ -20,27 +20,6 @@ const users = new mongoose.Schema({
     }
 });
 
-// Authenticate user
-// users.statics.authenticate = function (email, password, callback) {
-//     user.findOne({ email: email }).exec(callback);
-// }
-
-// Validate user session
-// users.statics.validate = function (email, callback) {
-//     user.findOne({ email: email })
-//         .exec(function (err, user) {
-//             if (err) {
-//                 return callback(err)
-//             } else if (!user) {
-//                 var err = new Error('User not found.');
-//                 err.status = 401;
-//                 return callback(err);
-//             }
-
-//             return callback(null, user)
-//         });
-// }
-
 // Hashing a password before saving it to the database
 users.pre('save', function (next) {
     var user = this;
