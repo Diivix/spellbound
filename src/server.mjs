03937097ca238245db4ef2cf60f5620efa91c3f6
Chris <chrisiivix@gmail.com>
2018-05-06 17:30:31 +1000

    Updated for production use.

--- src/server.mjs
+++ src/server.mjs
@@ -1,9 +1,5 @@
-import fs from 'fs';
-import http from 'http';
 import app from './app';
 
-const server = http.createServer(app)
-
-server.listen(process.env.PORT, function () {
-    console.log('Express server listening on port ' + process.env.SSL_PORT);
+app.listen(process.env.PORT, function () {
+    console.log('Express server listening on port ' + process.env.PORT);
 });
