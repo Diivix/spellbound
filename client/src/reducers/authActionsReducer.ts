28560c36ba871240a94ed847567d97fd596704be
Chris <chrisiivix@gmail.com>
2018-09-23 11:10:28 +1000

    Pending Actions works.

--- client/src/reducers/authActionsReducer.ts
+++ client/src/reducers/authActionsReducer.ts
@@ -2,18 +2,14 @@ import { AuthFail } from 'actions/common/types';
 import { TypedReducer } from 'redoodle';
 import { SignIn, SignOut } from '../actions/authentication/types';
 
-function createReducer() {
-  return TypedReducer.builder<boolean>()
-    .withHandler(SignIn.TYPE, () => {
-      return true;
-    })
-    .withHandler(SignOut.TYPE, () => {
-        return false;
-      })
-    .withHandler(AuthFail.TYPE, () => {
-      return false;
-    })
-    .build();
-}
-
-export const authReducer = createReducer();
+export const authReducer = TypedReducer.builder<boolean>()
+  .withHandler(SignIn.TYPE, () => {
+    return true;
+  })
+  .withHandler(SignOut.TYPE, () => {
+    return false;
+  })
+  .withHandler(AuthFail.TYPE, () => {
+    return false;
+  })
+  .build();
--- client/src/reducers/pendingActionsReducer.ts
+++ client/src/reducers/pendingActionsReducer.ts
@@ -1,15 +1,15 @@
 import { Fail, InProgress } from 'actions/common/types';
-import { setWith, TypedReducer } from 'redoodle';
+import { TypedReducer } from 'redoodle';
 
 export const pendingActionsReducer = TypedReducer.builder<number>()
-  .withHandler(InProgress.TYPE, (state) => {
-    return setWith(state, state + 1);
+  .withHandler(InProgress.TYPE, state => {
+    return state + 1;
   })
-  .withHandler(Fail.TYPE, (state) => {
-    return setWith(state, state > 0 ? state - 1 : 0)
+  .withHandler(Fail.TYPE, state => {
+    return state > 0 ? state - 1 : 0;
   })
-  .withDefaultHandler((state) => {
+  .withDefaultHandler(state => {
     // All other actions completed, either by success or failure
-    return setWith(state, state > 0 ? state - 1 : 0)
+    return state > 0 ? state - 1 : 0;
   })
-  .build()
+  .build();
