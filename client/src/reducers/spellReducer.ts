d3a77345d18380af10d1695cd57a007a27909466
Chris <chrisiivix@gmail.com>
2018-09-23 11:13:21 +1000

    Spells work.

--- client/src/reducers/spellReducer.ts
+++ client/src/reducers/spellReducer.ts
@@ -1,15 +1,15 @@
 import { ISpellData } from 'models';
-import { setWith, TypedReducer } from 'redoodle';
+import { TypedReducer } from 'redoodle';
 import { GetSpell, GetSpells, SetFilters } from '../actions/spells/types';
 
 export const spellReducer = TypedReducer.builder<ISpellData>()
   .withHandler(GetSpell.TYPE, (state, payload) => {
-    return setWith(state, { currentSpell: payload.spell });
+    return Object.assign({}, state, { currentSpell: payload.spell });
   })
   .withHandler(GetSpells.TYPE, (state, payload) => {
-    return setWith(state, { spells: payload.spells, filters: payload.filters });
+    return Object.assign({}, state, { spells: payload.spells, filters: payload.filters });
   })
   .withHandler(SetFilters.TYPE, (state, payload) => {
-    return setWith(state, { appliedFilters: payload.filters });
+    return Object.assign({}, state, { appliedFilters: payload.filters });
   })
   .build();
