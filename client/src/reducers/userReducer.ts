d9b59c0a314ecf2fe5a90ccde068bd16c0cc6e2d
Chris <chrisiivix@gmail.com>
2018-09-23 11:14:52 +1000

    User works.

--- client/src/reducers/userReducer.ts
+++ client/src/reducers/userReducer.ts
@@ -1,18 +1,18 @@
 import { IUserData } from 'models';
-import { setWith, TypedReducer } from 'redoodle';
+import { TypedReducer } from 'redoodle';
 import { CreateCharacter, DeleteCharacter, GetUser, UpdateCharacter } from '../actions/user/types';
 
 export const userReducer = TypedReducer.builder<IUserData>()
   .withHandler(GetUser.TYPE, (state, payload) => {
-    return setWith(state, payload.user);
+    return Object.assign({}, state, payload.user);
   })
   .withHandler(CreateCharacter.TYPE , (state, payload) => {
-    return setWith(state, payload.user);
+    return Object.assign({}, state, payload.user);
   })
   .withHandler(UpdateCharacter.TYPE , (state, payload) => {
-    return setWith(state, payload.user);
+    return Object.assign({}, state, payload.user);
   })
   .withHandler(DeleteCharacter.TYPE , (state, payload) => {
-    return setWith(state, payload.user);
+    return Object.assign({}, state, payload.user);
   })
   .build()
