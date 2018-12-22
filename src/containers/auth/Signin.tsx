99a5c3294211637ddea9a1811b7c668a9f8992aa
Chris <chrisiivix@gmail.com>
2018-12-22 13:12:58 +1100

    sb-text-center

--- src/containers/auth/Signin.tsx
+++ src/containers/auth/Signin.tsx
@@ -53,8 +53,8 @@ class SigninComponent extends React.Component<IProps, IState> {
     const margin = { marginBottom: '5px' };
 
     return (
-      <div className="sb-grid sb-center-h sb-center-v sb-celled-internally" style={{ height: '100vh' }}>
-        <div className="sb-row">
+      <div className="sb-grid sb-center-v sb-center-h sb-celled-internally" style={{ height: '100vh' }}>
+        <div className="sb-row sb-center-v">
           <div className="sb-col">
             <CssTransitionGroup
               transitionName="sb-signin-image"
@@ -63,14 +63,18 @@ class SigninComponent extends React.Component<IProps, IState> {
               transitionEnterTimeout={2000}
               transitionLeaveTimeout={2000}
             >
-              <div className="sb-grid sb-center-h sb-text-center" style={{ marginTop: '5%'}}>
+              <div className="sb-grid sb-center-h sb-text-center" style={{ marginTop: '5%' }}>
                 <div className="sb-row">
                   <div className="sb-col">
                     <i style={{ color: '#f21e1a' }} className="ra ra-fire-symbol ra-5x sb-icon sb-icon--circle" />
                   </div>
+                </div>
+                <div className="sb-row">
                   <div className="sb-col">
                     <i style={{ color: '#fcf41b' }} className="ra ra-lightning-bolt ra-5x sb-icon sb-icon--circle" />
                   </div>
+                </div>
+                <div className="sb-row">
                   <div className="sb-col">
                     <i style={{ color: '#24ccd8' }} className="ra ra-snowflake ra-5x sb-icon sb-icon--circle" />
                   </div>
@@ -80,7 +84,7 @@ class SigninComponent extends React.Component<IProps, IState> {
           </div>
           <div className="sb-col">
             <h2>Welcome to Spellbound</h2>
-            <form onClick={this.handleSubmit} style={{maxWidth: "350px"}}>
+            <form onClick={this.handleSubmit} style={{ maxWidth: '350px' }}>
               <FormGroup>
                 <InputGroup
                   id="email-input"
