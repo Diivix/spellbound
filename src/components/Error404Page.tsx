032a2851e32d183132929fe98968bf31b4c298d1
Chris <chrisiivix@gmail.com>
2019-01-03 00:13:33 +1100

    Updated character spells undesired state.

--- src/components/Error404Page.tsx
+++ src/components/Error404Page.tsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 class Error404Page extends React.Component {
   public render() {
     return (
-      <div className="sb-grid sb-center-h sb-center-v" style={{ marginTop: '5%' }}>
-        <div className="sb-row">
+      <div className="sb-grid sb-center-h sb-center-v">
+        <div className="sb-row sb-margin-top-40">
           <div className="sb-col">
             <i style={{ color: '#f21e1a' }} className="ra ra-fire-symbol ra-5x sb-icon sb-icon--circle" />
           </div>
@@ -15,7 +15,7 @@ class Error404Page extends React.Component {
             <i style={{ color: '#24ccd8' }} className="ra ra-snowflake ra-5x sb-icon sb-icon--circle" />
           </div>
         </div>
-        <div className="sb-row">
+        <div className="sb-row sb-margin-top-30">
           <div className="sb-col sb-center-text">
             <h2>These are not the spells you're looking for.</h2>
           </div>
--- src/components/characters/CharacterEquipped.tsx
+++ src/components/characters/CharacterEquipped.tsx
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import * as React from 'react';
+import { isNull } from 'util';
 
 interface IProps {
   heading: string;
@@ -23,7 +24,22 @@ class CharacterEquippedComponent extends React.Component<IProps, {}> {
         </div>
         <div className="sb-row">
           <div className="sb-col">
-            <div className="sb-card-group">{this.props.cards} </div>
+             {isNull(this.props.cards) || this.props.cards.length === 0 ? (
+              <div className="sb-grid sb-center-h sb-center-v">
+                <div className="sb-row sb-margin-top-30">
+                  <div className="sb-col">
+                    <i className="ra ra-lightning-storm ra-5x sb-icon sb-icon--circle" />
+                  </div>
+                </div>
+                <div className="sb-row sb-margin-top-30">
+                  <div className="sb-col sb-center-text">
+                    <p>No spells have been equipped.</p>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="sb-card-group">{this.props.cards} </div>
+            )}
           </div>
         </div>
       </div>
